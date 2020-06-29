b9a9c7a7f2e4d72e1b3551a858b74565fa609857
kseniyapanshina <kseniyapanshinana@gmail.com>
2020-06-29 16:10:16 +0300

    First

---
+++ src/test/java/com/pflb/learning/UxCrowdTest.java
@@ -0,0 +1,12 @@
+package com.pflb.learning;
+
+
+import org.junit.jupiter.api.Test;
+
+public class UxCrowdTest {
+
+    @Test
+    public void firstTest() {
+        DriverManager.getDriver().get("http://test.uxcrowd.ru");
+    }
+}
