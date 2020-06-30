c64d273128b496beb6df835268c766c9fc597687
ioann <ioanntrapeznikov@gmail.com>
2020-06-30 10:20:21 +0300

    Maybe

---
+++ src/main/java/pages/ProfilePage.java
@@ -0,0 +1,109 @@
+package pages;
+
+import com.pflb.learning.DriverManager;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ProfilePage {
+
+    public ProfilePage() {
+        PageFactory.initElements(DriverManager.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//*[@id=\"top\"]/div[2]/div/div[2]/div[1]/div[1]/span")
+    private WebElement buttonCreateTest;//Элемент , который даёт понять нам что мы перешли на страницу профиля
+
+    @FindBy(xpath = "//*[@id=\"top\"]/div[2]/div/div[1]/aside/section[2]/div")
+    private WebElement profileSettings;
+
+    @FindBy(xpath = "//*[@id=\"top\"]/div[2]/div/div[2]/div[2]/div[2]/div/div/div[1]/div[1]/input")
+    private WebElement checkProfileLogin;
+
+    @FindBy(xpath = "//*[@id=\"top\"]/div[2]/div/div[2]/div[2]/div[2]/div/div/div[1]/div[2]/input")
+    private WebElement checkProfileEmail;
+    //Added new test
+    @FindBy(xpath = "//*[@id=\"top\"]/div[2]/div/div[1]/aside/section[1]")
+    private WebElement buttonAddNewTest;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/div[2]/div[1]/div[2]/label/input")
+    private WebElement nameTest;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/div[2]/div[3]/div[2]/label/input")
+    private WebElement url;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/div[2]/button")
+    private WebElement goToChangePersons;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/div[2]/div[1]/div[2]/div[1]/label[1]/div")
+    private WebElement elementWhichWait;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/div[2]/div[2]/ul/li/div/div[1]/div[2]/label/input")
+    private WebElement nameSegment;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/div[2]/button[2]")
+    private WebElement goToQuest;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/div[2]/div/div[1]/label/textarea")
+    private WebElement setQuest;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/div[2]/div/div[1]/button")
+    private WebElement addedQuest;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/div[2]/div/button[2]")
+    private WebElement checkAndRun;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/div[2]/div/section[4]/div/button")
+    private WebElement runFreeTest;
+
+    @FindBy(xpath = "//*[@id=\"modal-root\"]/div/div/div/div/div/div[1]")
+    private WebElement actualTextAfterCreateTest;
+
+    public String actualText() {
+        return actualTextAfterCreateTest.getText();
+    }
+
+    public void descriptionTest(String testName, String site, String segment, String quest) {
+        nameTest.sendKeys(testName);
+        url.sendKeys(site);
+        new WebDriverWait(DriverManager.getDriver(), 10);
+        goToChangePersons.click();
+        new WebDriverWait(DriverManager.getDriver(), 10).until(ExpectedConditions.visibilityOf(elementWhichWait));
+        nameSegment.sendKeys(segment);
+        goToQuest.click();
+        new WebDriverWait(DriverManager.getDriver(), 10).until(ExpectedConditions.visibilityOf(setQuest));
+        setQuest.sendKeys(quest);
+        new WebDriverWait(DriverManager.getDriver(), 10).until(ExpectedConditions.elementToBeClickable(addedQuest));
+        addedQuest.click();
+        new WebDriverWait(DriverManager.getDriver(), 10).until(ExpectedConditions.elementToBeClickable(checkAndRun));
+        checkAndRun.click();
+        new WebDriverWait(DriverManager.getDriver(), 10).until(ExpectedConditions.elementToBeClickable(runFreeTest));
+        runFreeTest.click();
+    }
+
+    public void pressButtonCreateNewTest() {
+        buttonCreateTest.click();
+    }
+
+    public boolean isDisplayedButtonCreateTest() {
+        return buttonCreateTest.isDisplayed();
+    }
+
+    public void checkProfileSettings() {
+        profileSettings.click();
+    }
+
+    public String getProfileLogin() {
+        new WebDriverWait(DriverManager.getDriver(), 10).until(ExpectedConditions.visibilityOf(checkProfileLogin));
+        return checkProfileLogin.getAttribute("value");
+    }
+
+    public String getProfileEmail() {
+        new WebDriverWait(DriverManager.getDriver(), 10).until(ExpectedConditions.visibilityOf(checkProfileEmail));
+        return checkProfileEmail.getAttribute("value");
+    }
+
+
+}
--- src/main/java/pages/StartPage.java
+++ src/main/java/pages/StartPage.java
@@ -4,12 +4,22 @@ import com.pflb.learning.DriverManager;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class StartPage {
+
+    private final String email = "@1secmail.com";
+
+    public StartPage() {
+        PageFactory.initElements(DriverManager.getDriver(), this);
+    }
+
+    //AUTH
     @FindBy(id = "header-lk-button")
     private WebElement loginButton;
 
-    @FindBy(id = "login")
+    @FindBy(css = "#login")
     private WebElement loginFiled;
 
     @FindBy(css = "[type=password]")
@@ -17,12 +27,55 @@ public class StartPage {
 
     @FindBy(css = "#form_auth button")
     private WebElement submitButton;
+    //REGISTRATION
+    @FindBy(css = "[for=registrationLk]")
+    private WebElement buttonRegistrationClient;
 
-    public StartPage() {
-        PageFactory.initElements(DriverManager.getDriver(), this);
+    @FindBy(xpath = "/html/body/div[1]/div/div/div/div/div[2]/section[2]/div[1]/button")
+    private WebElement toConsumer;
+
+    @FindBy(xpath = "//*[@id=\"name\"]")
+    private WebElement yourName;
+
+    @FindBy(xpath = "//*[@id=\"position\"]")
+    private WebElement yourPosition;
+
+    @FindBy(xpath = "//*[@id=\"company\"]")
+    private WebElement yourCompany;
+
+    @FindBy(xpath = "//*[@id=\"emails\"]")
+    private WebElement yourEmail;
+
+    @FindBy(xpath = "//*[@id=\"phoneNumber\"]")
+    private WebElement yourPhone;
+
+    @FindBy(xpath = "//*[@id=\"form_register_customer\"]/button")
+    private WebElement buttonAgree;
+
+
+    public void authClient(String login, String password) {
+        loginButton.click();
+        new WebDriverWait(DriverManager.getDriver(), 10);
+        loginFiled.sendKeys(login);
+        passwordField.sendKeys(password);
+        submitButton.click();
     }
 
-    public void open() {
-        DriverManager.getDriver().get("http://");
+    public void open(String url) {
+        DriverManager.getDriver().get(url);
+    }
+
+    public void registrationNewClient(String name, String position, String company, String mail, String phone) {
+        loginButton.click();
+        new WebDriverWait(DriverManager.getDriver(), 10).until(ExpectedConditions.elementToBeClickable(buttonRegistrationClient));
+        buttonRegistrationClient.click();
+        new WebDriverWait(DriverManager.getDriver(),10).until(ExpectedConditions.elementToBeClickable(toConsumer));
+        toConsumer.click();
+        yourName.sendKeys(name);
+        yourPosition.sendKeys(position);
+        yourCompany.sendKeys(company);
+        yourEmail.sendKeys(mail + email );
+        yourPhone.sendKeys(phone);
+        buttonAgree.click();
     }
 }
