ea03f3a03b68e7425107a98e811bf4186b079bf8
ioann <ioanntrapeznikov@gmail.com>
2020-06-30 10:19:27 +0300

    Change

--- src/main/java/com/pflb/learning/DriverManager.java
+++ src/main/java/com/pflb/learning/DriverManager.java
@@ -9,6 +9,7 @@ import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 
 public class DriverManager {
@@ -21,23 +22,29 @@ public class DriverManager {
 
     public static WebDriver getDriver() {
         if (driver != null) return driver;
+
         InputStream inputStream = DriverManager.class.getClassLoader().getResourceAsStream("config.properties");
         Properties properties = new Properties();
         try {
             properties.load(inputStream);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException((e));
         }
         String hubUrlString = properties.getProperty("selenium.hub.url");
+        String browserName = properties.getProperty("selenium.browser.name");
+        int globalTimeWait = Integer.parseInt(properties.getProperty("global.time.wait.second"));
         URL hubUrl;
         try {
-            hubUrl = new URL(hubUrlString);
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
+            hubUrl= new URL(hubUrlString);
         }
+        catch (MalformedURLException e) {
+            throw  new RuntimeException(e); }
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("browserName", "chrome");
+        capabilities.setCapability("browserName", browserName);
         driver = new RemoteWebDriver(hubUrl, capabilities);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(globalTimeWait, TimeUnit.SECONDS);
         return driver;
     }
 }
