6dc798230814f390a23bcc28e0263e732b7374f5
kseniyapanshina <kseniyapanshinana@gmail.com>
2020-07-02 22:47:31 +0300

    regAndAuthPage

--- src/test/java/com/pflb/learning/tests/CreateNewTest.java
+++ src/test/java/com/pflb/learning/tests/CreateNewTest.java
@@ -3,6 +3,7 @@ package com.pflb.learning.tests;
 import com.pflb.learning.testpages.AuthTestPage;
 import com.pflb.learning.testpages.CreateNewTestPage;
 import com.pflb.learning.testpages.RegistrationTestPage;
+import com.pflb.learning.testpages.RegAndAuthPage;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -12,6 +13,7 @@ public class CreateNewTest {
     private final RegistrationTestPage registrationTestPage = new RegistrationTestPage();
     private final AuthTestPage authTestPage = new AuthTestPage();
     private final CreateNewTestPage createNewTestPage = new CreateNewTestPage();
+    private final RegAndAuthPage regAndAuthPage = new RegAndAuthPage();
 
     private static final String BASEURL = "http://test.uxcrowd.ru";
     private static final String EXCEPTEDTEXT = "Ура! Тест создан и отправлен пользователям.";
@@ -20,7 +22,8 @@ public class CreateNewTest {
     @BeforeTest
     public void authClient() {
         registrationTestPage.open(BASEURL);
-        authTestPage.authClient();
+        regAndAuthPage.registrationClient();
+        regAndAuthPage.authClient();;
     }
 
     @Test
